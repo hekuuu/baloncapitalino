66170f0b8f4653093208c6eb8f7c8dc47ce33c77
Codigo <helenndaniela2005@gmail.com>
2025-05-18 23:09:42 -0500

    tarjeta, slider y imagenes

--- apps/urls.py
+++ apps/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import baloncapitalino_view
+from .views import baloncapitalino_view, femenino_view, unica_view, kids_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', baloncapitalino_view, name='baloncapitalino'),
+    path('femenino/', femenino_view, name='femenino'),
+    path('unica/', unica_view, name='unica'),
+    path('kids/', kids_view, name='kids'),
 ]
 
 if settings.DEBUG:
--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import MiModelo
 
 def baloncapitalino_view(request):
     return render(request, "baloncapitalino.html")
+
+def femenino_view(request):
+    equipos = MiModelo.objects.all()
+    return render(request, 'femenino.html', {'equipos': equipos})
+def unica_view(request):
+    return render(request, 'unica.html')
+def kids_view(request):
+    return render(request, 'kids.html')
